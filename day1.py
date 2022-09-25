65358c993b299d363f91374baa4eb9de7cc46640
jegini <jeginilokesh012022@gmail.com>
2022-09-25 12:21:06 +0530

    .git delete

---
+++ day1.py
@@ -0,0 +1,17 @@
+# we save python file by using the extensions .py
+# single comment is mention with "#" symbol
+# multi comment is mention in python by using tripple quotes
+"""
+This is a comment
+written in
+more than just one line
+"""
+# simple ouutput program
+print("hello wordl")
+# accept input dynamically from the uses
+x=input("enter your first name");
+y=input("enter your first name");
+print("full name ",x,y);
+# //this is for git comment merge
+#doubt every statement is end with semicolan ani,
+#note python follow indentation it did not consist flower braces
